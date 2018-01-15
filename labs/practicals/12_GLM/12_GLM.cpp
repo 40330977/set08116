1412d22e0ca65c2a8c90c1e7e418b483811dd738
Ewles <40330977@live.napier.ac.uk>
2018-01-15 15:15:17 +0000

    some work

--- labs/practicals/12_GLM/12_GLM.cpp
+++ labs/practicals/12_GLM/12_GLM.cpp
@@ -10,4 +10,42 @@
 using namespace std;
 using namespace glm;
 
-int main() {}
+int main() {
+
+	vec2 u(1.0f, 0.0f);
+	vec2 z(1.0f, 0.0f);
+
+	vec3 v(1.0f, 0.0f, 0.0f);
+	vec3 w(1.0f, 0.0f, 0.0f);
+
+	vec4 x(1.0f, 0.0f, 0.0f, 1.0f);
+	vec4 y(1.0f, 0.0f, 0.0f, 1.0f);
+
+	vec3 a(vec2(1.0f, 0.0f), 0.0f);//2d
+	vec4 b(vec2(1.0f, 0.0f), 0.0f, 1.0f);
+
+	vec2 c(vec3(1.0f, 0.0f, 0.0f));//3d
+	vec4 d(vec3(1.0f, 0.0f, 0.0f), 1.0f);
+
+	vec2 e(vec4(1.0f, 0.0f, 0.0f, 1.0f));//4d
+	vec3 f(vec4(1.0f, 0.0f, 0.0f, 1.0f));
+
+	vec2 g = u + z;
+	vec2 h = u - z;
+
+	vec3 i = v + w;
+	vec3 j = v - w;
+
+	vec4 k = x + y;
+	vec4 l = x - y;
+
+	vec2 m = 5.0f*u;
+	vec2 n = z / 5.0f;
+
+	vec3 p = 5.0f*v;
+	vec3 q = w / 5.0f;
+
+	vec4 r = 5.0f*x;
+	vec4 s = y / 5.0f;
+}
+
