39c330a4a10c7b67e602b9d0dcb5e590c6d10a81
Ewles <40330977@live.napier.ac.uk>
2018-02-22 15:30:29 +0000

    target cam

--- labs/practicals/37_Target_Camera/37_Target_Camera.cpp
+++ labs/practicals/37_Target_Camera/37_Target_Camera.cpp
@@ -61,23 +61,31 @@ bool update(float delta_time) {
   // *********************************
   // Use keyboard to change camera location
   // 1 - (50, 10, 50)
-
+	if (glfwGetKey(renderer::get_window(), 'Q')) {
+		cam.set_position(vec3(50.0f, 10.0f, 50.0f));
+	}
 
 
   // 2 - (-50, 10, 50)
-
+	if (glfwGetKey(renderer::get_window(), 'W')) {
+		cam.set_position(vec3(-50.0f, 10.0f, 50.0f));
+	}
 
 
   // 3 - (-50, 10, -50)
-
+	if (glfwGetKey(renderer::get_window(), 'E')) {
+		cam.set_position(vec3(-50.0f, 10.0f, -50.0f));
+	}
 
 
   // 4 - (50, 10, -50)
-
+	if (glfwGetKey(renderer::get_window(), 'R')) {
+		cam.set_position(vec3(50.0f, 10.0f, -50.0f));
+	}
 
 
   // Update the camera
-
+	cam.update(delta_time);
   // *********************************
 
   return true;
