72a94e931d077f602ceac7caa56fe622c486c3c5
Ewles <40330977@live.napier.ac.uk>
2018-03-13 20:52:23 +0000

    CW

--- labs/coursework/src/main.cpp
+++ labs/coursework/src/main.cpp
@@ -5,24 +5,20 @@ using namespace std;
 using namespace graphics_framework;
 using namespace glm;
 
-geometry geom;
+map<string, mesh> meshes;
 effect eff;
+texture tex;
 target_camera cam;
+vector<point_light> points(4);
+vector<spot_light> spots(5);
 
 bool load_content() {
-  // Create triangle data
-  vector<vec3> positions{vec3(0.0f, 1.0f, 0.0f), vec3(-1.0f, -1.0f, 0.0f), vec3(1.0f, -1.0f, 0.0f)
-
-  };
-  // Colours
-  vector<vec4> colours{vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f), vec4(1.0f, 0.0f, 0.0f, 1.0f)};
-  // Add to the geometry
-  geom.add_buffer(positions, BUFFER_INDEXES::POSITION_BUFFER);
-  geom.add_buffer(colours, BUFFER_INDEXES::COLOUR_BUFFER);
-
-  // Load in shaders
-  eff.add_shader("shaders/basic.vert", GL_VERTEX_SHADER);
-  eff.add_shader("shaders/basic.frag", GL_FRAGMENT_SHADER);
+  // Create meshes
+	meshes["plane"] = mesh(geometry_builder::create_plane());
+	meshes["box1"] = mesh(geometry_builder::create_box());
+	meshes["box2"] = mesh(geometry_builder::create_box());
+	meshes["disk"] = mesh(geometry_builder::create_disk(20));
+	meshes["cylinder"] = mesh(geometry_builder::create_cylinder(20, 20));
   // Build effect
   eff.build();
 
@@ -41,18 +37,45 @@ bool update(float delta_time) {
 }
 
 bool render() {
-  // Bind effect
-  renderer::bind(eff);
-  // Create MVP matrix
-  mat4 M(1.0f);
-  auto V = cam.get_view();
-  auto P = cam.get_projection();
-  auto MVP = P * V * M;
-  // Set MVP matrix uniform
-  glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
-  // Render geometry
-  renderer::render(geom);
-  return true;
+	// Render meshes
+	for (auto &e : meshes) {
+		auto m = e.second;
+		// Bind effect
+		renderer::bind(eff);
+		// Create MVP matrix
+		auto M = m.get_transform().get_transform_matrix();
+		auto V = cam.get_view();
+		auto P = cam.get_projection();
+		auto MVP = P * V * M;
+		// Set MVP matrix uniform
+		glUniformMatrix4fv(eff.get_uniform_location("MVP"), // Location of uniform
+			1,                               // Number of values - 1 mat4
+			GL_FALSE,                        // Transpose the matrix?
+			value_ptr(MVP));                 // Pointer to matrix data
+											 // *********************************
+											 // Set M matrix uniform
+		glUniformMatrix4fv(eff.get_uniform_location("M"), 1, GL_FALSE, value_ptr(M));
+		// Set N matrix uniform - remember - 3x3 matrix
+		glUniformMatrix3fv(eff.get_uniform_location("N"), 1, GL_FALSE, value_ptr(m.get_transform().get_normal_matrix()));
+		// Bind material
+		renderer::bind(m.get_material(), "mat");
+		// Bind point lights
+		renderer::bind(points, "points");
+		// Bind spot lights
+		renderer::bind(spots, "spots");
+		// Bind texture
+		renderer::bind(tex, 0);
+		// Set tex uniform
+		glUniform1i(eff.get_uniform_location("tex"), 0);
+		// Set eye position- Get this from active camera
+		vec3 eye_pos = cam.get_position();
+		glUniform3fv(eff.get_uniform_location("eye_pos"), 1, value_ptr(eye_pos));
+		// Render mesh
+		renderer::render(m);
+		// *********************************
+	}
+
+	return true;
 }
 
 void main() {
